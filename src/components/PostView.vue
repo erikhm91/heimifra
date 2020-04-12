07a2827c2a58da8cb6ca8dc3968f62427b030cc4
Erik Houge Mathisen <erikhougem@gmail.com>
2020-04-12 19:16:39 +0200

    added location filter functionality

--- src/components/PostView.vue
+++ src/components/PostView.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="row mt-2">
     <div class="col-md-8 offset-md-2 col-12">
-      
-      <div>Min plassering: {{this.userlat}}, {{this.userlong}}</div>
-      
+      <!-- <div>Min plassering: {{this.userlat}}, {{this.userlon}}</div> -->
+
+      <div>Avstand i km: {{range}}</div>
+      <!-- <input type="range" name="location" id="distanceslider"> -->
+      <vue-slider v-model="range" :min="1"></vue-slider>
+      <div>min posisjon: {{userlat}},{{userlon}}</div>
+      <!-- <div>test avstand i km: {{getDistance(userlat, userlon, 59.9396, 10.6715)}}</div> -->
+      <!-- <div>avstand fra meg selv data: {{distance}}</div>
+      <div>avstand fra meg selv data: {{distance}}</div> -->
+
+      <!-- <div>avstand fra meg selv ny formel: {{getDistanceFromUser(59, 9, 59, 9.01)}}</div> -->
+
       <div class="text-center">
         <button v-b-modal="'modal'" class="btn btn-primary">+ Opprett ny handleliste</button>
       </div>
-
 
       <b-modal :id="'modal'" :title="'Opprett ny liste'" :hide-footer="true" centered>
         <post-creator @complete="closeModal('modal')"></post-creator>
@@ -20,6 +28,8 @@
           v-bind:key="i"
           :post="post"
         >
+        
+        <div v-if="post.loc" @click="printGeolocation(post.loc)">{{getDistance(userlat, userlon, post.loc.latitude, post.loc.longitude)}} kilometer fra deg</div>
           <!-- Modal handled inside post-reply -->
           <post-reply :post="post"></post-reply>
         </home-store-post>
@@ -35,43 +45,73 @@ import db from "@/firebase/init";
 import PostCreator from "@/components/posts/PostCreator.vue";
 import PostReply from "@/components/posts/PostReply.vue";
 import { mapGetters, mapActions } from "vuex";
+import VueSlider from "vue-slider-component";
+import "vue-slider-component/theme/default.css";
 export default {
   data() {
     return {
       userlat: null,
-      userlong: null
-    }
+      userlon: null,
+      range: 10,
+      distance: 0
+    };
   },
-  computed:{ 
-      ...mapGetters(['postsNotOwn','postArray', 'activeUser']),
-      filteredPosts() {
-        return this.postArray.filter(post => post.uid != this.activeUser.uid)
-      }
+  computed: {
+    ...mapGetters(["postsNotOwn", "postArray", "activeUser"]),
+    filteredPosts() {
+      return this.postArray.filter(post => post.uid != this.activeUser.uid);
+    }
   },
   components: {
     homeStorePost: Post,
     PostCreator,
     postReply: PostReply,
+    VueSlider
   },
-  async mounted() {
-    console.log('postArray: ',this.postArray)
-    
+  mounted() {
+    console.log("postArray: ", this.postArray);
+
     //get user geolocation
-    console.log('here we identify the geolocation of user')
+    console.log("here we identify the geolocation of user");
     //check if browser supports geolocation
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((pos) => {
-        this.userlat = pos.coords.latitude
-        this.userlong = pos.coords.longitude
-        console.log("geolocation of user in callback: ", this.userlat, this.userlong)
-        //TODO trigger search on database
-      })
-    }
+      navigator.geolocation.getCurrentPosition(pos => {
+        this.userlat = pos.coords.latitude;
+        this.userlon = pos.coords.longitude;
+        console.log(
+          "geolocation of user in callback: ",
+          this.userlat,
+          this.userlon
+        );
 
+        let distance = this.getDistanceFromLatLonInKm(
+          this.userlat,
+          this.userlon,
+          this.userlat,
+          this.userlon
+        );
+        console.log("distance: ", distance);
+        //TODO trigger search on database
+      });
+    }
   },
   methods: {
-    closeModal(id){
-      this.$bvModal.hide(id)
+    closeModal(id) {
+      this.$bvModal.hide(id);
+    },
+    getDistance(lat1, lon1, lat2, lon2) {
+      var p = 0.017453292519943295; // Math.PI / 180
+      var c = Math.cos;
+      var a =
+        0.5 -
+        c((lat2 - lat1) * p) / 2 +
+        (c(lat1 * p) * c(lat2 * p) * (1 - c((lon2 - lon1) * p))) / 2;
+
+      let distance = 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
+      return Math.round(distance)
+    },
+    printGeolocation(loc) {
+      console.log("geoloc", loc)
     }
   }
 };
